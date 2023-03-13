1756e50196f320c7faacf52a5297c06df7d49122
Michael Gerdemann <michael@gerdemann.me>
2023-03-13 13:55:25 +0100

    feat: Add CHECK_SERVICE

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,11 @@ FROM alpine:3.10
 COPY entrypoint.sh /entrypoint.sh
 
 ARG RANCHER_COMPOSE_VERSION=0.12.5
+ARG RANCHER_CLI_VERSION=0.6.14
 
-RUN apk add --quiet --no-cache ca-certificates curl unzip
+RUN apk add --quiet --no-cache ca-certificates curl unzip jq
 RUN curl -sSL "https://github.com/rancher/rancher-compose/releases/download/v${RANCHER_COMPOSE_VERSION}/rancher-compose-linux-amd64-v${RANCHER_COMPOSE_VERSION}.tar.gz" | tar -xzp -C /usr/local/bin/ --strip-components=2
+RUN curl -sSL "https://releases.rancher.com/cli/v0.6.14/rancher-linux-amd64-v${RANCHER_CLI_VERSION}.tar.gz" | tar -xzp -C /usr/local/bin/ --strip-components=2
 RUN rm -rf /var/cache/*
 
 ENTRYPOINT [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,26 @@ export RANCHER_COMPOSE_FILE="$4"
 export DOCKER_COMPOSE_FILE="$5"
 export STACK_NAME="$6"
 export ENV_FILE="$7"
+export CHECK_SERVICE="$8"
 
-rancher-compose \
-    --file ${DOCKER_COMPOSE_FILE} \
-    --rancher-file ${RANCHER_COMPOSE_FILE} \
-    --project-name "${STACK_NAME}" \
-    --env-file=${ENV_FILE} \
-    up -d --confirm-upgrade --force-recreate
+if [ "$CHECK_SERVICE" != "" ]; then
+    state=$(rancher inspect $CHECK_SERVICE | jq -r .state)
+
+    if [ "$state" = "active" ]; then
+        rancher-compose \
+            --file ${DOCKER_COMPOSE_FILE} \
+            --rancher-file ${RANCHER_COMPOSE_FILE} \
+            --project-name "${STACK_NAME}" \
+            --env-file=${ENV_FILE} \
+            up -d --confirm-upgrade --force-recreate
+    else
+        echo "skipped=true" >> $GITHUB_OUTPUT
+    fi
+else
+    rancher-compose \
+        --file ${DOCKER_COMPOSE_FILE} \
+        --rancher-file ${RANCHER_COMPOSE_FILE} \
+        --project-name "${STACK_NAME}" \
+        --env-file=${ENV_FILE} \
+        up -d --confirm-upgrade --force-recreate
+fi
